073bd75c0b466260a8f876c129f6e105dc18afed
swanand <swanandgadwe@gmail.com>
2022-03-22 19:32:23 +0530

    hackathon_project

---
+++ my website/questions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: " If you fall for a phishing scam, what should you do to limit the damage ?",
+    answer: "Change any compromised passwords",
+    options: [
+      "Delete the phishing email.",
+      "Unplug the computer. This will get rid of any malware",
+      "Change any compromised passwords",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "A predator online who goes through all your details on the internet to find you is called",
+    answer: "Cyberbullying",
+    options: [
+      "Cyberbullying",
+      "Cyberstalking",
+      "Cybergrooming",
+      "Cybermeeting"
+    ]
+  },
+    {
+    numb: 3,
+    question: " You get a text message from a vendor who asks you to click on a link to renew your password so that you can log in to its website. You should:",
+    answer: "Pick up the phone and call the vendor, using a phone number you know to be correct, to confirm that the request is real.",
+    options: [
+      "Reply to the text to confirm that you really need to renew your password.",
+      "Pick up the phone and call the vendor, using a phone number you know to be correct, to confirm that the request is real.",
+      "Click on the link. If it takes you to the vendor’s website, then you’ll know it’s not a scam",
+      "both a and c"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of these best describes how criminals start ransomware attacks?",
+    answer: "All of the above",
+    options: [
+      "Sending a scam email with links or attachments that put your data and network at risk",
+      "Getting into your server through vulnerabilities and installing malware.",
+      "Using infected websites that automatically download malicious software to your computer or mobile device.",
+      "All of the above"
+    ]
+  },
+  {
+  numb: 5,
+  question: "Which of the following is used to identify a website user and can be a vulnerablity to your privacy if not cleared reguraly",
+  answer: "cookies",
+  options: [
+    "Pop-ups",
+    "plug-ins",
+    "cookies",
+    "scripts"
+  ]
+  },
+  {
+    numb: 6,
+    question: "If you encounter a ransomware attack, the first thing you should do is pay the ransom. True or False?",
+    answer: "False",
+    options: [
+      "True",
+      "False",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 7,
+    question: "Setting your software to auto-update is one way you can help protect your business from ransomware. True or False? ",
+    answer: "True",
+    options: [
+      "True",
+      "False",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 8,
+    question: "An email from your boss asks for the name, address, and credit card information of the company’s top clients. The email says it’s urgent and to please reply right away. You should reply right away. True or False?",
+    answer: "False",
+    options: [
+      "True",
+      "False",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 9,
+    question: "Email authentication can help protect against phishing attacks. True or False?",
+    answer: "",
+    options: [
+      "True",
+      "False",
+      "",
+      ""
+    ]
+  },
+    {
+      numb: 10,
+      question: "Use a firewall to prevent hackers from attacking your computer remotely.",
+      answer: "True",
+      options: [
+        "True",
+      "False",
+      "",
+      ""
+      ]
+  },
+
+];
